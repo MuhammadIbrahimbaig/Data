9dbfe7504f38b0a9d11fb123c1bedf224c2eb3ab
MuhammadIbrahimbaig <138751972+MuhammadIbrahimbaig@users.noreply.github.com>
2024-03-05 15:25:52 +0500

    sett

--- Estate/NiceAdmin/login.php
+++ Estate/NiceAdmin/login.php
@@ -64,13 +64,13 @@ session_start();
             <form method="post" action="logincheck.php">
               <div class="row">
                 <div class="col-md-12 form-group">
-                  <input type="email" name="user_email" class="form-control" placeholder="Enter your email" required>
+                  <input type="text" name="user" class="form-control" placeholder="Enter your email" required>
                 </div>
                 <br>
                 <br>
                 <br>
                  <div class="col-md-12 form-group mt-3 mt-md-0">
-                  <input type="password" class="form-control" name="user_password"  placeholder="Enter your password" required>
+                  <input type="password" class="form-control" name="pass"  placeholder="Enter your password" required>
                 </div>
                 <br>
                 <br>
@@ -85,28 +85,7 @@ session_start();
     </section>
 <?php
 include("footer.php");
-// authentication
-include("config.php");
 
-
-if(isset($_POST['login'])){
-  $email = $_POST['user'];
-  $password = $_POST['pass'];
-
-  $query = "SELECT * from `users` where `user_name` = '$email' AND `passwordd` = '$password'";
-  $result = mysqli_query($conn, $query);
-  $data = mysqli_fetch_assoc($result);
-  if($data){
-    $_SESSION['user_id'] = $data['user_id'];
-    $_SESSION['user_name'] = $data['user_name'];
-    // $_SESSION['role'] = $data['role'];
-    if($_SESSION['user_name'] =='admin'){
-    header('location:index.php');
-    }
- 
-  }
-
-}
 
 ?>
 
