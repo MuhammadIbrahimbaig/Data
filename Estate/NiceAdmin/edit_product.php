635b9dd4b57963ef06603b3608dc87495fc28c63
MuhammadIbrahimbaig <138751972+MuhammadIbrahimbaig@users.noreply.github.com>
2024-03-17 20:26:03 +0500

    update

---
+++ Estate/NiceAdmin/edit_product.php
@@ -0,0 +1,20 @@
+<?php
+include("header.php");
+?>
+
+<div class="container">
+    <form class="form w-50 mt-5 mx-auto" action=""></form>
+<h1 class="text-center  mx-auto mt-5">Edit Product</h1>
+<input   value="Id"  class="form-control p-1 mt-2" type="text" name="id"> 
+<input value="prodname"  class="form-control p-1  mt-2" type="text" name="prodname" >
+<input value="prodprice" class="form-control p-1  mt-2" type="text" name="prodprice" >
+<input value="prodimage"  class="form-control p-1  mt-2" type="file" name="prodimage">
+<input value="Prod_Desc" class="form-control p-1 mt-2"type="text" name="Prod_Desc">
+</div>
+
+
+
+
+<?php
+include("footer.php");
+?>
