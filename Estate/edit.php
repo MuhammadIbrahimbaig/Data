d4ae55ed8a60dec7552545d5afed1940070c3d68
jiyaamir <javeriya.amirhassan2007@gmail.com>
2024-04-18 18:54:32 +0500

    good hugya

--- Estate/edit.php
+++ Estate/edit.php
@@ -1,6 +1,7 @@
 <?php
+session_start();
     include("config.php");
-    $id = $_POST["id"];
+    $id = $_SESSION["id"];
     // $user_id = $_POST["user_id"];
     $user_name = $_POST["user_name"];
     $email = $_POST["email"];
@@ -9,12 +10,13 @@
     $Phone = $_POST["Phone"];
     // $role = $_POST["role"];
 
-$edit_query = "UPDATE `users` SET `user_name`='$user_name',`email`='$email',`password`='$password',`address`='$address',`Phone`='$Phone' WHERE user_id = $id";
+$edit_query = "UPDATE `users` SET `user_name`='$user_name',`email`='$email',`passwordd`='$password',`address`='$address',`Phone`='$Phone' WHERE user_id = $id";
+$edit_result = mysqli_query($conn,$edit_query);
 
 
 if($edit_result){
     echo "value update";
-    header("Location: index.php");
+    // header("Location: index.php");
 }
 else {
     echo "not update";
