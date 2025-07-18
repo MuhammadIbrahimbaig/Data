e999aa79ce616c792ec86718e1178dfa94f52940
asp <asp@aptech.com>
2025-07-18 09:42:57 +0500

    class

--- express/index.js
+++ express/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+let db = require("./dbconnection")
 const app = express();
 const port = 4001;
 
@@ -10,6 +10,9 @@ app.use(express.json());
 const route = require("./Routes/route");
 app.use("/", route); // ✅ Must be used to activate /save
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
-});
+db().then(()=>{
+
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
+})
